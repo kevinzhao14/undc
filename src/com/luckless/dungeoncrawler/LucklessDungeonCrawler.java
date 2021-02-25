a7d5723b2bbd5349797b9425f3fea7d6a2c49242
Colten Webb <coltenwebb@gmail.com>
2021-02-24 19:39:19 -0500

    Add entrypoint for application + way to change scenes

---
+++ src/com/luckless/dungeoncrawler/LucklessDungeonCrawler.java
@@ -0,0 +1,33 @@
+package com.luckless.dungeoncrawler;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class LucklessDungeonCrawler extends Application {
+    private static LucklessDungeonCrawler INSTANCE;
+
+    private Stage stage;
+    private Scene scene;
+
+    public void start(Stage stage) {
+        INSTANCE = this;
+
+        this.stage = stage;
+        this.scene = new Scene(new Pane());
+
+        stage.setTitle("Luckless Dungeon Crawler");
+        setScene(scene);
+
+        stage.show();
+    }
+
+    /**
+     * Used to change the scene.
+     * @param scene scene to change to
+     */
+    public static void setScene(Scene scene) {
+        INSTANCE.stage.setScene(scene);
+    }
+}
