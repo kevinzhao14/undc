19236d5327360b385dd6173be17aee2bbaba62d9
mharbola3 <mharbola3@gatech.edu>
2021-02-26 20:11:38 -0500

    InitPlayerConfigScreen is now subclass of abstract class GameState

--- src/com/luckless/dungeoncrawler/InitPlayerConfigScreen.java
+++ src/com/luckless/dungeoncrawler/InitPlayerConfigScreen.java
@@ -13,10 +13,11 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-public class InitPlayerConfigScreen {
+public class InitPlayerConfigScreen extends GameState {
     //Application Window dimensions
     private int windowHeight;
     private int windowWidth;
+    private Scene scene;
     private Button nextButton;
     private TextField playerNameEntry;
 
@@ -32,23 +33,28 @@ public class InitPlayerConfigScreen {
         this.windowWidth = width;
         this.windowHeight = height;
         nextButton = new Button("Next");
+        nextButton.setAlignment(Pos.CENTER);
+        this.scene = this.createScene();
     }
 
-    public Scene getScene() {
+    private Scene createScene() {
         Label enterPlayerName = new Label("Enter player name:");
         playerNameEntry = new TextField();
+        enterPlayerName.setAlignment(Pos.TOP_LEFT);
+        playerNameEntry.setAlignment(Pos.TOP_LEFT);
 
         Label selectDifficulty = new Label("Select difficulty:");
+        selectDifficulty.setAlignment(Pos.TOP_LEFT);
         ToggleGroup difficultyGroup = new ToggleGroup();
         ArrayList<RadioButton> difficultyButtons = new ArrayList<>(
                 InitPlayerConfigScreen.Difficulty.values().length);
-
         for (int i = 0; i < InitPlayerConfigScreen.Difficulty.values().length; i++) {
             difficultyButtons.add(new RadioButton(InitPlayerConfigScreen.Difficulty.values()[i].name()));
             difficultyButtons.get(i).setToggleGroup(difficultyGroup);
         }
 
         Label selectStarterWeapon = new Label("Select starter weapon:");
+        selectStarterWeapon.setAlignment(Pos.TOP_LEFT);
         ToggleGroup weaponGroup = new ToggleGroup();
         ArrayList<RadioButton> weaponButtons = new ArrayList<>(
                 InitPlayerConfigScreen.Weapon.values().length);
@@ -58,39 +64,51 @@ public class InitPlayerConfigScreen {
         }
 
         GridPane root = new GridPane();
-        root.setVgap(5);
-        root.setHgap(5);
+        root.setVgap(20.0);
+        root.setHgap(20.0);
+        root.setAlignment(Pos.CENTER);
 
         //Add player name row
-        root.add(enterPlayerName, 0, 0);
-        root.add(playerNameEntry, 1, 0);
+        VBox row1 = new VBox(5);
+        row1.getChildren().addAll(enterPlayerName, playerNameEntry);
+        root.add(row1, 0, 0);
+        root.setColumnSpan(row1, 2);
 
-        //Add difficulty row
-        root.add(selectDifficulty, 0, 1);
-        selectDifficulty.setAlignment(Pos.TOP_CENTER);
-        VBox difficultyVBox = new VBox(5);
-        for (RadioButton btn : difficultyButtons) {
-            difficultyVBox.getChildren().add(btn);
+        //Create difficulty row
+        GridPane row2 = new GridPane();
+        row2.setVgap(10.0);
+        row2.setHgap(10.0);
+        row2.add(selectDifficulty, 0, 0);
+        for (int i = 0; i < difficultyButtons.size(); i++) {
+            row2.add(difficultyButtons.get(i), 1, i);
         }
-        root.add(difficultyVBox, 1, 1);
-
-        //Add starting weapon row
-        root.add(selectStarterWeapon, 0, 2);
-        VBox weaponVBox = new VBox(5);
-        for (RadioButton btn : weaponButtons) {
-            weaponVBox.getChildren().add(btn);
+        row2.add(selectStarterWeapon, 0, difficultyButtons.size() + 1);
+        for (int i = 0; i < weaponButtons.size(); i++) {
+            row2.add(weaponButtons.get(i), 1, i + difficultyButtons.size() + 1);
         }
-        root.add(weaponVBox, 1, 2);
 
-        root.add(nextButton, 1, 3);
+        //Add row2 to root
+        root.add(row2, 0, 1);
+        root.setColumnSpan(row2, 2);
+
+        VBox row3 = new VBox(5);
+        //Adjust button size
+        nextButton.setMaxWidth(100.0);
+
+        row3.getChildren().add(nextButton);
+        row3.setAlignment(Pos.CENTER_RIGHT);
+
+        //Add row3 to root
+        root.add(row3, 1, 2);
 
         Scene scene = new Scene(root, this.windowWidth, this.windowHeight);
         return scene;
     }
 
-    public Button getNextButton() { return this.nextButton; }
+    //Implements the getScene() method from abstract class GameState
+    public Scene getScene() {return this.scene;}
 
-    public String getEnteredPlayerName() {
-        return this.playerNameEntry.getText();
-    }
+    public Button getNextButton() {return this.nextButton;}
+
+    public String getEnteredPlayerName() {return this.playerNameEntry.getText();}
 }
