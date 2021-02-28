7900c63a8b2554080b3a52061d82b255fc78ece7
Colten Webb <coltenwebb@gmail.com>
2021-02-27 21:26:23 -0500

    Add test example and junit to pom.xml

---
+++ src/test/java/TestExample.java
@@ -0,0 +1,13 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestExample {
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void works() {
+        System.out.println("aksjdhflaksjdf");
+    }
+}
