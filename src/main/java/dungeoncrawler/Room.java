70dd7f2cb0057387800017c4628fda7017a4445e
mharbola3 <mharbola3@gatech.edu>
2021-03-07 20:46:01 -0500

    Remove styleSheetPath and rename attributes and getter methods

--- src/main/java/dungeoncrawler/Room.java
+++ src/main/java/dungeoncrawler/Room.java
@@ -1,36 +1,33 @@
 package dungeoncrawler;
 
 public class Room {
-    private int windowHeight;
-    private int windowWidth;
-    private Obstacle[] roomObstacles;
+    private int height;
+    private int width;
+    private Obstacle[] obstacles;
     private Door topDoor;
     private Door bottomDoor;
     private Door rightDoor;
     private Door leftDoor;
-    private RoomType roomType;
-    private String roomStyleSheetPath;
+    private RoomType type;
 
     public Room (int height, int width, Obstacle[] roomObstacles,
                  Door topDoor, Door bottomDoor, Door rightDoor,
-                 Door leftDoor, RoomType roomType, String styleSheetPath) {
-        this.windowHeight = height;
-        this.windowWidth = width;
-        this.roomObstacles = roomObstacles;
-        //this.roomExits = roomExits
+                 Door leftDoor, RoomType roomType) {
+        this.height = height;
+        this.width = width;
+        this.obstacles = roomObstacles;
         this.topDoor = topDoor;
         this.bottomDoor = bottomDoor;
         this.rightDoor = rightDoor;
         this.leftDoor = leftDoor;
-        this.roomType = roomType;
-        this.roomStyleSheetPath = styleSheetPath;
+        this.type = roomType;
     }
 
     public int getRoomHeight() {
-        return this.windowHeight;
+        return this.height;
     }
     public int getRoomWidth() {
-        return this.windowWidth;
+        return this.width;
     }
     public Door getTopDoor() {
         return this.topDoor;
@@ -44,13 +41,10 @@ public class Room {
     public Door getLeftDoor() {
         return this.leftDoor;
     }
-    public RoomType getRoomType() {
-        return this.roomType;
+    public RoomType getType() {
+        return this.type;
     }
     public Obstacle[] getObstacles() {
-        return this.roomObstacles;
-    }
-    public String getStyleSheetPath() {
-        return this.roomStyleSheetPath;
+        return this.obstacles;
     }
 }
