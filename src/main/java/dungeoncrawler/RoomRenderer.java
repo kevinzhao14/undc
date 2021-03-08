cb03703b7dd777abf24eb5cb4f64a9275afd0abc
insaan21 <ishaan.guha@gmail.com>
2021-03-07 20:47:36 -0500

    update room renderer

--- src/main/java/dungeoncrawler/RoomRenderer.java
+++ src/main/java/dungeoncrawler/RoomRenderer.java
@@ -9,26 +9,26 @@ public class RoomRenderer {
         Group root = new Group();
         Scene scene = new Scene(root, room.getRoomWidth(), room.getRoomHeight());
         for (Obstacle obstacle : room.getObstacles()) {
-            Rectangle r = new Rectangle(obstacle.getXPos(), obstacle.getYPos(), obstacle.getWidth(), obstacle.getHeight());
+            Rectangle r = new Rectangle(obstacle.getX(), obstacle.getY(), obstacle.getWidth(), obstacle.getHeight());
             root.getChildren().add(r);
         }
         if (room.getTopDoor() != null) {
-            Rectangle r = new Rectangle(room.getTopDoor().getXPos(), room.getTopDoor().getYPos(), room.getTopDoor().getWidth(), room.getTopDoor().getHeight());
+            Rectangle r = new Rectangle(room.getTopDoor().getX(), room.getTopDoor().getY(), room.getTopDoor().getWidth(), room.getTopDoor().getHeight());
             root.getChildren().add(r);
         }
         if (room.getRightDoor() != null) {
-            Rectangle r = new Rectangle(room.getRightDoor().getXPos(), room.getRightDoor().getYPos(), room.getRightDoor().getWidth(), room.getRightDoor().getHeight());
+            Rectangle r = new Rectangle(room.getRightDoor().getX(), room.getRightDoor().getY(), room.getRightDoor().getWidth(), room.getRightDoor().getHeight());
             root.getChildren().add(r);
         }
         if (room.getBottomDoor() != null) {
-            Rectangle r = new Rectangle(room.getBottomDoor().getXPos(), room.getBottomDoor().getYPos(), room.getBottomDoor().getWidth(), room.getBottomDoor().getHeight());
+            Rectangle r = new Rectangle(room.getBottomDoor().getX(), room.getBottomDoor().getY(), room.getBottomDoor().getWidth(), room.getBottomDoor().getHeight());
             root.getChildren().add(r);
         }
         if (room.getLeftDoor() != null) {
-            Rectangle r = new Rectangle(room.getLeftDoor().getXPos(), room.getLeftDoor().getYPos(), room.getLeftDoor().getWidth(), room.getLeftDoor().getHeight());
+            Rectangle r = new Rectangle(room.getLeftDoor().getX(), room.getLeftDoor().getY(), room.getLeftDoor().getWidth(), room.getLeftDoor().getHeight());
             root.getChildren().add(r);
         }
-        scene.getStylesheets().add(room.getStyleSheetPath());
+        scene.getStylesheets().add(room.getType().name() + ".css");
         return scene;
     }
 }
