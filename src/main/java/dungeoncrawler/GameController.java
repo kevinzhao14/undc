88bc3535ddc8869c25fd9cb5688e4e5a72fdfea7
mharbola3 <mharbola3@gatech.edu>
2021-03-08 21:23:14 -0500

    Begin implementation of GameController and GameSettings class

--- src/main/java/dungeoncrawler/GameController.java
+++ src/main/java/dungeoncrawler/GameController.java
@@ -1,6 +1,7 @@
 package dungeoncrawler;
 
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 
 import java.util.Timer;
@@ -8,29 +9,105 @@ import java.util.TimerTask;
 
 public class GameController {
     private Timer timer;
+
+    //TODO: Create Controls object in Controller
+    private Controls controls;
+    private Room room;
+    private Scene scene;
+    private Node player;
+    private double posX, posY;
     private double velX, velY;
     private double accelX, accelY;
     private boolean isRunning;
     private long ticks;
     private double totalTime;
-    private Node player;
+
+    public GameController(Room room, Scene scene, Node player) {
+        if (room == null) {
+            throw new IllegalArgumentException(
+                    "Cannot assign null Room reference to GameController instance"
+            );
+        }
+        if (scene == null) {
+            throw new IllegalArgumentException(
+                    "Cannot assign null Scene reference to GameController instance"
+            );
+        }
+        if (player == null) {
+            throw new IllegalArgumentException(
+                    "Cannot assign null Player reference to GameController instance"
+            );
+        }
+        this.room = room;
+        this.scene = scene;
+        this.player = player;
+        this.timer = new Timer();
+        this.controls = new Controls();
+    }
 
     public void start() {
+        //Reset the game
+        reset();
 
+        //Handle key events
+        scene.setOnKeyPressed(e -> {
+            handleKey(e.getCode(), true);
+        });
+        scene.setOnKeyPressed(e -> {
+            handleKey(e.getCode(), false);
+        });
+    }
+
+    private void reset() {
+        posX = room.getStartX();
+        posY = room.getStartY();
+        velX = 0.0;
+        velY = 0.0;
+        accelX = 0.0;
+        accelY = 0.0;
+        isRunning = false;
+        ticks = 0;
+        totalTime = 0;
     }
 
     public void pause() {
+        if (isRunning) {
+            timer.cancel();
+        } else {
+            startTimer();
+        }
 
+        isRunning = !isRunning;
     }
 
-    public void resume() {
-
+    private void startTimer() {
+        timer.schedule(new GameRunner(), 0, 1000 / GameSettings.FPS);
     }
 
-    public void handleKey(KeyCode key, boolean isPress) {
+    private void handleKey(KeyCode keyCode, boolean isPress) {
+        if (!isRunning) {
+            return;
+        }
+        String key = keyCode.toString();
 
+        if (key.equals(controls.getKey("up"))) {
+            accelY += (isPress && accelY < GameSettings.ACCEL ? 1 : -1) * GameSettings.ACCEL;
+            accelY = round(accelY);
+        } else if (key.equals(controls.getKey("down"))) {
+            accelY += (isPress && accelY > -GameSettings.ACCEL ? -1 : 1) * GameSettings.ACCEL;
+            accelY = round(accelY);
+        } else if (key.equals(controls.getKey("right"))) {
+            accelX += (isPress && accelX < GameSettings.ACCEL ? 1 : -1) * GameSettings.ACCEL;
+            accelX = round(accelX);
+        } else if (key.equals(controls.getKey("left"))) {
+            accelX += (isPress && accelX > -GameSettings.ACCEL ? -1 : 1) * GameSettings.ACCEL;
+            accelX = round(accelX);
+        }
     }
 
+    private double round(double number) {
+        return Math.round(number * GameSettings.PRECISION) / GameSettings.PRECISION;
+    }
 
     class GameRunner extends TimerTask {
         public void run() {
---
+++ src/main/java/dungeoncrawler/GameSettings.java
@@ -0,0 +1,11 @@
+package dungeoncrawler;
+
+public static class GameSettings {
+    public static final int FPS = 60;
+    public static final double ACCEL = 1.0;
+    public static final double MAXVEL = 5.0;
+    public static final double FRICTION = ACCEL;
+    //pixels per unit
+    public static final double PPU = 5.0;
+    public static final int PRECISION = 10000;
+}
