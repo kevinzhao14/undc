f6ea15aa07d378fa2769ced8a103bd02a7524f60
Kevin Zhao <kevin@randume.org>
2021-03-10 17:57:18 -0500

    Implement door checking and changing rooms, add descriptive comments

--- src/main/java/dungeoncrawler/GameController.java
+++ src/main/java/dungeoncrawler/GameController.java
@@ -3,6 +3,8 @@ package dungeoncrawler;
 import javafx.scene.image.ImageView;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -22,7 +24,7 @@ public class GameController {
     private long ticks;
     private double totalTime;
 
-    public GameController(Room room, Scene scene, ImageView player) {
+    public GameController(ImageView player) {
         if (room == null) {
             throw new IllegalArgumentException(
                     "Cannot assign null Room reference to GameController instance"
@@ -38,16 +40,14 @@ public class GameController {
                     "Cannot assign null Player reference to GameController instance"
             );
         }
-        this.room = room;
-        this.scene = scene;
         this.player = player;
         this.timer = new Timer();
         this.controls = new Controls();
     }
 
-    public void start() {
-        //Reset the game
-        reset();
+    public void start(Room room) {
+        //Render room
+        setRoom(room);
 
         //Handle key events
         scene.setOnKeyPressed(e -> {
@@ -56,6 +56,14 @@ public class GameController {
         scene.setOnKeyPressed(e -> {
             handleKey(e.getCode(), false);
         });
+    }
+
+    private void setRoom(Room newRoom) {
+        room = newRoom;
+        scene = RoomRenderer.drawRoom(newRoom, player);
+        reset();
+        isRunning = true;
+        Controller.setScene(scene);
     }
 
     private void reset() {
@@ -122,9 +130,7 @@ public class GameController {
             boolean isValidPos = checkPos(posX, posY, newPosX, newPosY);
             if (isValidPos) {
                 movePlayer(newPosX, newPosY);
-                if (atDoor(posX, posY, newPosX, newPosY)) {
-
-                }
+                checkDoors(posX, posY, newPosX, newPosY);
                 posX = newPosX;
                 posY = newPosY;
             }
@@ -143,26 +149,28 @@ public class GameController {
         }
 
         private boolean inRange(Obstacle o, double x, double y, double newX, double newY) {
+            /* Checks if the object is in range of the player's movement
+             *          _________
+             *          |[]     |
+             *          |  \    |
+             *          |   \   |
+             *          |    \  |
+             *          |     []|
+             *          ---------
+             */
+
             if (o.getX() + o.getWidth() < Math.min(x, newX)
-                && o.getX() > Math.max(x, newX) + GameSettings.PLAYER_WIDTH) {
+                || o.getX() > Math.max(x, newX) + GameSettings.PLAYER_WIDTH) {
                 return false;
             }
             if (o.getY() + o.getHeight() < Math.min(y, newY)
-                && o.getY() > Math.max(y, newY) + GameSettings.PLAYER_HEIGHT) {
+                || o.getY() > Math.max(y, newY) + GameSettings.PLAYER_HEIGHT) {
                 return false;
             }
             return true;
         }
 
-        //TODO insert comments below in javadoc
-        //intX -> point of intersection in x-axis
-        //intY -> point of intersection in y-axis
-        private boolean checkObstacle(Obstacle o, double intX, double intY,
-                                      boolean left, boolean bottom) {
-            return true;
-        }
-
-        private boolean atDoor(double x, double y, double newX, double newY) {
+        private void checkDoors(double x, double y, double newX, double newY) {
             //TODO: implement getDoors() method in Room class
             Door[] doors = {
                 room.getTopDoor(),
@@ -177,18 +185,71 @@ public class GameController {
                 if (!inRange(d, x, y, newX, newY)) {
                     continue;
                 }
-                //Get equation for intersection
-                double[]
-                //Check if player is inside door
+                //player movement direction
+                boolean moveRight = x < newX;
+                boolean moveUp = y < newY;
 
-                if (checkObstacle(d, ))
+                //Get equation for intersection
+                double[] playerEquation = equation(x, y, newX, newY);
+                double[] intersects = getIntersect(d, playerEquation[0], playerEquation[1], moveUp, moveRight);
+
+                //player intersects door
+                if (intersects != null) {
+                    Room newRoom = d.getGoesTo();
+                    setRoom(newRoom);
+                }
             }
+        }
+
+        private double[] getIntersect(Obstacle o, double m, double b, boolean moveUp, boolean moveRight) {
+            /* Calculate x-coordinate intersection point on the y-axis
+             *
+             *          v obstacle
+             *          ------------------
+             *          |     \          |
+             *          |      \         |
+             *          --------*---------
+             *                  ^\
+             *                    \ < movement vector
+             *
+             * We have equation y = mx + b from playerEquation, which is the vector/line for the player's movement.
+             * Since the door is within the rectangle bound by the player's vector as calculated above, we know that
+             * any collisions are on the vector and not past it.
+             * We pass in the y value of either the top or bottom of the obstacle, depending on if the player is
+             * moving downward or upward. If horizontal, it defaults to downward, but it doesn't matter.
+             * We then substitute the y in "y = mx + b", along with the m and b values obtained earlier, and solve
+             * for x, resulting in an equation "x = (y - b) / m"
+             */
+            double intY = (o.getY() + o.getHeight() - b) / m;
+            if (moveUp) {
+                intY = (o.getY() - GameSettings.PLAYER_HEIGHT - b) / m;
+            }
+
+            //check if intersect is on the obstacle
+            if (intY <= o.getX() + o.getWidth() || intY + GameSettings.PLAYER_WIDTH >= o.getX()) {
+                double coord = m * intY + b;
+                return new double[]{intY, coord};
+            }
+
+            //Calculate y-coordinate intersection point on the x-axis, using y = mx + b
+            double intX = m * (o.getX() + o.getWidth()) + b;
+            if (moveRight) { //intersect is the right side of the player to the left side of the obstacle
+                intX = m * (o.getX() - GameSettings.PLAYER_WIDTH) + b;
+            }
+
+            //check if intersect is on the obstacle
+            if (intX <= o.getY() + o.getHeight() || intX + GameSettings.PLAYER_HEIGHT >= o.getY()) {
+                double coord = (intX - b) / m;
+                return new double[]{coord, intX};
+            }
+
+            return null;
         }
 
         private void movePlayer(double x, double y) {
             //Update player position
             player.setX(getPx(posX));
-            player.setY(scene.getHeight() - getPx(posY));
+            player.setY(scene.getHeight() - getPx(posY) - GameSettings.PLAYER_HEIGHT);
 
             //Move camera, if needed
             moveCamera();
@@ -196,20 +257,6 @@ public class GameController {
 
         //Implement if necessary
         private void moveCamera() {
-        }
-
-        /**
-         *
-         * @param m slope of equation 1
-         * @param b y-intercept of equation 1
-         * @param a slope of equation 2
-         * @param c y-intercept of equation 2
-         * @return x and y coordinates of intersection
-         */
-        private double[] intersect(double m, double b, double a, double c) {
-            double x = (b - c) / (a - m);
-            double y = m * x + b;
-            return new double[]{x, y};
         }
 
         private double[] equation(double x0, double y0, double x1, double y1) {
--- src/main/java/dungeoncrawler/GameSettings.java
+++ src/main/java/dungeoncrawler/GameSettings.java
@@ -1,9 +1,42 @@
 package dungeoncrawler;
 
-public static class GameSettings {
+public class GameSettings {
+    /*
+     *      Player and Object coordinate system
+     *
+     * -----------------------------------
+     *                                   |
+     *      --------- < getY() + height  |
+     *      |       |                    |
+     *      |       |                    |
+     *      *-------- < getY()           |
+     *      ^       ^                    |
+     *      getX()  getX() + width       |
+     *                                   |
+     * v origin                          |
+     * *----------------------------------
+     *
+     *      However, JavaFX does things a little differently:
+     *
+     * *----------------------------------
+     * ^ origin                          |
+     *                                   |
+     *      getX()  getX() + width       |
+     *      v       v                    |
+     *      --------- < getY();          |
+     *      |       |                    |
+     *      |       |                    |
+     *      *-------- < getY() + height  |
+     *                                   |
+     * -----------------------------------
+     *
+     * Because of this, we need to convert our Y coordinates to JavaFX's Y coordinates.
+     * Please note: All calculations except for actually moving JavaFX nodes should be calculated using
+     * system 1.
+     */
     public static final int FPS = 60;
     public static final double ACCEL = 1.0;
-    public static final double MAXVEL = 5.0;
+    public static final double MAX_VEL = 5.0;
     public static final double FRICTION = ACCEL;
     //pixels per unit
     public static final double PPU = 5.0;
