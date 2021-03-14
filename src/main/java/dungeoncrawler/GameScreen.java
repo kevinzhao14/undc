34eff46dd037f85818ee597596e488c9cf52fb7d
Trenton Wong <51030496+trenton-wong@users.noreply.github.com>
2021-03-13 19:39:25 -0500

    Update HUD to display gold

--- src/main/java/dungeoncrawler/GameScreen.java
+++ src/main/java/dungeoncrawler/GameScreen.java
@@ -1,11 +1,18 @@
 package dungeoncrawler;
 
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class GameScreen extends GameState {
@@ -14,20 +21,22 @@ public class GameScreen extends GameState {
     private ImageView player;
     private DungeonLayout dungeonLayout;
     private Room room;
-    private Pane hud;
+    private BorderPane hud;
 
     public GameScreen(int width, int height) {
         player = new ImageView("playerPlaceholder.png");
         player.setFitHeight(GameSettings.PLAYER_HEIGHT * GameSettings.PPU);
         player.setFitWidth(GameSettings.PLAYER_WIDTH * GameSettings.PPU);
 
+        /*
         //TODO: Implement layoutgenerator
         Room tempRoom2 = new Room(300, 300, 50, 50, new Obstacle[5], RoomType.EMPTYROOM);
         Door tempDoor = new Door(200, 300, 20, 10, tempRoom2, DoorOrientation.TOP);
         Room tempRoom = new Room(400, 400, 100, 100, new Obstacle[5], RoomType.EMPTYROOM);
         tempRoom.setTopDoor(tempDoor);
+        */
 
-        dungeonLayout = new DungeonLayout(tempRoom, tempRoom2); //LayoutGenerator.generateLayout();
+        dungeonLayout = LayoutGenerator.generateLayout();
         room = dungeonLayout.getStartingRoom();
 
         scene = new Scene(new Pane(), width, height);
@@ -46,8 +55,28 @@ public class GameScreen extends GameState {
         game.setPlayer(player);
         room = newRoom;
         StackPane root = new StackPane();
-        hud = new Pane();
-        root.getChildren().addAll(hud, RoomRenderer.drawRoom(scene, room, player));
+        hud = new BorderPane();
+
+        Label goldLabel = new Label();
+        HBox lowerHUD = new HBox();
+        hud.setBottom(lowerHUD);
+        lowerHUD.setAlignment(Pos.CENTER);
+        lowerHUD.setPadding(new Insets(10, 10, 10, 10));
+        lowerHUD.getChildren().add(goldLabel);
+        switch (Controller.getDataManager().getDifficulty()) {
+            case EASY:
+                goldLabel.setText("Gold: 300");
+                break;
+            case MEDIUM:
+                goldLabel.setText("Gold: 200");
+                break;
+            default:
+                goldLabel.setText("Gold: 100");
+                break;
+        }
+        goldLabel.setTextFill(Color.WHITE);
+
+        root.getChildren().addAll(RoomRenderer.drawRoom(scene, room, player), hud);
         scene.setRoot(root);
     }
 
