586b64a8781a1af8640783364190e1aae953e991
insaan21 <ishaan.guha@gmail.com>
2021-03-13 19:36:39 -0500

    Add doors to Layout Generator

--- src/main/java/dungeoncrawler/LayoutGenerator.java
+++ src/main/java/dungeoncrawler/LayoutGenerator.java
@@ -43,7 +43,7 @@ public class LayoutGenerator {
         // down path
         roomGrid[GRID_WIDTH / 2][GRID_HEIGHT / 2 - 1] = new Room(400, 400, 100, 100, new Obstacle[5], RoomType.EMPTYROOM);
         coords = generateRoom(roomGrid, GRID_WIDTH / 2, GRID_HEIGHT / 2 - 1);
-        int downPath = (int)(Math.random() * 7) + 4
+        int downPath = (int)(Math.random() * 7) + 4;
         for (int i = 0; i < downPath - 1; i++) {
             coords = generateRoom(roomGrid, coords[0], coords[1]);
         }
@@ -61,9 +61,20 @@ public class LayoutGenerator {
          */
 
         // create doors
-        for (int i = 0; i < GRID_WIDTH; i++) {
-            for (int j = 0; j < GRID_HEIGHT; j++) {
-
+        for (int i = 1; i < GRID_WIDTH - 1; i++) {
+            for (int j = 1; j < GRID_HEIGHT - 1; j++) {
+                if (roomGrid[i + i][j] != null) {
+                    roomGrid[i][j].setRightDoor(new Door(480,245,20,10, roomGrid[i + 1][j], DoorOrientation.RIGHT));
+                }
+                if (roomGrid[i - 1][j] != null) {
+                    roomGrid[i][j].setLeftDoor(new Door(100,245,20,10, roomGrid[i - 1][j], DoorOrientation.LEFT));
+                }
+                if (roomGrid[i][j + 1] != null) {
+                    roomGrid[i][j].setBottomDoor(new Door(200,500,20,10, roomGrid[i][j + 1], DoorOrientation.BOTTOM));
+                }
+                if (roomGrid[i][j - 1] != null) {
+                    roomGrid[i][j].setTopDoor(new Door(200,300,20,10, roomGrid[i][j - 1], DoorOrientation.TOP));
+                }
             }
         }
 
