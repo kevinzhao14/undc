15790f13ce1fa84df8a3dae213c97c61da9e42f2
mharbola3 <mharbola3@gatech.edu>
2021-04-03 20:03:16 -0400

    Add PotionType enum

---
+++ src/main/java/dungeoncrawler/objects/PotionType.java
@@ -0,0 +1,11 @@
+package dungeoncrawler.objects;
+
+/**
+ * PotionType enum for defining characteristics
+ * of a Potion object's modifier attribute
+ *
+ * @author Manas Harbola
+ */
+public enum PotionType {
+    HEALTH, ATTACK;
+}
