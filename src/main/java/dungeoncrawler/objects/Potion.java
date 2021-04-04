d80bed8851d7e41b243b50f554d4c3b783ceb910
mharbola3 <mharbola3@gatech.edu>
2021-04-04 19:31:33 -0400

    Define abstract copy() method

--- src/main/java/dungeoncrawler/objects/Potion.java
+++ src/main/java/dungeoncrawler/objects/Potion.java
@@ -10,16 +10,20 @@ public class Potion extends Item {
     private PotionType type;
     private double modifier;
 
-    public Potion(String path, String itemName, int stackSize,
+    public Potion(String name, String path, int stackSize,
                   boolean isDroppable, PotionType potionType, double potionModifier) {
-        super(new Image(path), itemName, stackSize, isDroppable);
+        super(new Image(path), name, stackSize, isDroppable);
         type = potionType;
         modifier = potionModifier;
     }
-
     public Potion(String path, String itemName, PotionType potionType) {
         super(new Image(path), itemName);
         type = potionType;
+    }
+
+    public Potion copy() {
+        return new Potion(getName(), getSprite().getUrl(), getMaxStackSize(),
+                isDroppable(), type, modifier);
     }
 
     public PotionType getType() {
